e76af6dfa6e6bbb3ce9f6873f37409e56949d118
Oiva Moisio <nimerib@hotmail.com>
2015-06-08 18:50:08 +0300

    Added qstring fetch and also added wears

--- car.cpp
+++ car.cpp
@@ -3,3 +3,40 @@
 Car::Car()
 {
 }
+
+QString Car::getFieldAsQString(CarSlots slot)
+{
+    switch (slot) {
+        case CAR_TRACK_NAME: return track_name_;
+        case CAR_SEASON: return QString::number(season_);
+        case CAR_POWER: return QString::number(car_stats_[CAR_POWER-stats_diff]);
+        case CAR_HANDLING: return QString::number(car_stats_[CAR_HANDLING-stats_diff]);
+        case CAR_ACCELERATION: return QString::number(car_stats_[CAR_ACCELERATION-stats_diff]);
+        case CAR_CHASSIS_LVL: return QString::number(part_lvl_[CAR_CHASSIS_LVL-lvl_diff_]);
+        case CAR_ENGINE_LVL: return QString::number(part_lvl_[CAR_ENGINE_LVL-lvl_diff_]);
+        case CAR_FRONT_WING_LVL: return QString::number(part_lvl_[CAR_FRONT_WING_LVL-lvl_diff_]);
+        case CAR_REAR_WING_LVL: return QString::number(part_lvl_[CAR_REAR_WING_LVL-lvl_diff_]);
+        case CAR_UNDERBODY_LVL: return QString::number(part_lvl_[CAR_UNDERBODY_LVL-lvl_diff_]);
+        case CAR_SIDEPODS_LVL: return QString::number(part_lvl_[CAR_SIDEPODS_LVL-lvl_diff_]);
+        case CAR_COOLING_LVL: return QString::number(part_lvl_[CAR_COOLING_LVL-lvl_diff_]);
+        case CAR_GEARBOX_LVL: return QString::number(part_lvl_[CAR_GEARBOX_LVL-lvl_diff_]);
+        case CAR_BRAKES_LVL: return QString::number(part_lvl_[CAR_BRAKES_LVL-lvl_diff_]);
+        case CAR_SUSPENSION_LVL: return QString::number(part_lvl_[CAR_SUSPENSION_LVL-lvl_diff_]);
+        case CAR_ELECTRONICS_LVL: return QString::number(part_lvl_[CAR_ELECTRONICS_LVL-lvl_diff_]);
+        case CAR_CHASSIS_WEAR: return QString::number(part_wear_[CAR_CHASSIS_WEAR-wear_diff]);
+        case CAR_ENGINE_WEAR: return QString::number(part_wear_[CAR_ENGINE_WEAR-wear_diff]);
+        case CAR_FRONT_WING_WEAR: return QString::number(part_wear_[CAR_FRONT_WING_WEAR-wear_diff]);
+        case CAR_REAR_WING_WEAR: return QString::number(part_wear_[CAR_REAR_WING_WEAR-wear_diff]);
+        case CAR_UNDERBODY_WEAR: return QString::number(part_wear_[CAR_UNDERBODY_WEAR-wear_diff]);
+        case CAR_SIDEPODS_WEAR: return QString::number(part_wear_[CAR_SIDEPODS_WEAR-wear_diff]);
+        case CAR_COOLING_WEAR: return QString::number(part_wear_[CAR_COOLING_WEAR-wear_diff]);
+        case CAR_GEARBOX_WEAR: return QString::number(part_wear_[CAR_GEARBOX_WEAR-wear_diff]);
+        case CAR_BRAKES_WEAR: return QString::number(part_wear_[CAR_BRAKES_WEAR-wear_diff]);
+        case CAR_SUSPENSION_WEAR: return QString::number(part_wear_[CAR_SUSPENSION_WEAR-wear_diff]);
+        case CAR_ELECTRONICS_WEAR: return QString::number(part_wear_[CAR_ELECTRONICS_WEAR-wear_diff]);
+    default:
+        break;
+    }
+
+    return QString();
+}
--- car.h
+++ car.h
@@ -1,18 +1,46 @@
 #ifndef CAR_H
 #define CAR_H
 
+#include <QString>
+
+enum CarSlots { CAR_TRACK_NAME = 0, CAR_SEASON, CAR_POWER, CAR_HANDLING, CAR_ACCELERATION,
+                CAR_CHASSIS_LVL, CAR_ENGINE_LVL, CAR_FRONT_WING_LVL, CAR_REAR_WING_LVL, CAR_UNDERBODY_LVL,
+                CAR_SIDEPODS_LVL, CAR_COOLING_LVL, CAR_GEARBOX_LVL, CAR_BRAKES_LVL, CAR_SUSPENSION_LVL, CAR_ELECTRONICS_LVL,
+                CAR_CHASSIS_WEAR, CAR_ENGINE_WEAR, CAR_FRONT_WING_WEAR, CAR_REAR_WING_WEAR, CAR_UNDERBODY_WEAR,
+                CAR_SIDEPODS_WEAR, CAR_COOLING_WEAR, CAR_GEARBOX_WEAR, CAR_BRAKES_WEAR, CAR_SUSPENSION_WEAR,
+                CAR_ELECTRONICS_WEAR};
+
 class Car
 {
     private:
-    static const char car_field_names_[] = {"Car.\"Name (Track)\"", "Car.Season", "Car.Power", "Car.Handling",
+    static const char car_lvl_names_[] = {"Car.\"Name (Track)\"", "Car.Season", "Car.Power", "Car.Handling",
                                            "Car.Acceleration", "Car.Chassis", "Car.Engine", "Car.\"Front Wing\"",
                                            "Car.\"Rear Wing\"", "Car.Underbody", "Car.Sidepods", "Car.Cooling",
                                            "Car.Gearbox", "Car.Brakes", "Car.Suspension", "Car.Electronics"};
+    static const char car_wear_names_[] = {"CarWear.\"Name (Track)\"", "CarWear.Season", "CarWear.\"Front Wing Wear\"",
+                                          "CarWear.\"Rear Wing Wear\"", "CarWear.\"Engine Wear\"", "CarWear.\"Brakes Wear\"",
+                                          "CarWear.\"Gearbox Wear\"", "CarWear.\"Suspension Wear\"", "CarWear.\"Chassis Wear\"",
+                                          "CarWear.\"Cooling Wear\"", "CarWear.\"Electronics Wear\"", "CarWear.\"Sidepods Wear\"",
+                                          "CarWear.\"Underbody Wear\""};
+
+    static const int stats_diff = 2;
+    static const int lvl_diff_ = 5;
+    static const int wear_diff = 16;
+
+    QString track_name_;
+    int season_;
+    int car_stats_[3];
+    int part_lvl_[11];
+    int part_wear_[11];
 
 public:
     Car();
 
-    static const char* const getFieldNames() { return car_field_names_; }
+    static const char* const getStatNames() { return car_lvl_names_; }
+    static const char* const getWearNames() { return car_wear_names_; }
+
+    // getters
+    QString getFieldAsQString(CarSlots slot);
 };
 
 #endif // CAR_H
