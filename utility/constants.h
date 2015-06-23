f02a78e33e38c85412fea03e0407184e58830ade
Oiva Moisio <nimerib@hotmail.com>
2015-06-23 20:37:54 +0300

    Added some constants.

--- utility/constants.h
+++ utility/constants.h
@@ -1,4 +1,20 @@
 #ifndef SETTINGSCONSTANTS_H
 #define SETTINGSCONSTANTS_H
 
+#include <array>
+
+#include <QString>
+
+namespace Settings {
+    const QString DatabaseTypeText = "database/type";
+    const QString DatabaseNameText = "database/name";
+    const QString DatabaseUserNameText =  "database/username";
+    const QString DatabasePasswordText = "database/password";
+}
+
+namespace General {
+    const QString ProgramName = "GproRaceCalculator";
+    const QString CompanyName = "Tasogare Soft";
+}
+
 #endif // SETTINGSCONSTANTS_H
